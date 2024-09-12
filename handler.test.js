b99908ae7ac92ed7b8bc7aa164cae81dcf593470
Tommycpg-ce6 <tommy.chuapg@gmail.com>
2024-09-12 21:04:30 +0800

    add

---
+++ handler.test.js
@@ -0,0 +1,14 @@
+/ handler.test.js
+const { add, subtract } = require('./handler');
+
+describe('handler functions', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(add(1, 2)).toBe(3);
+  });
+
+  test('subtracts 5 - 3 to equal 2', () => {
+    expect(subtract(5, 3)).toBe(2);
+  });
+
+  // Add more tests as needed
+});
